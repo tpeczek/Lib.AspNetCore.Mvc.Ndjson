5f54b5d3be379862ffb39c1dc3a7cdf8f098f3fb
Tomasz Pęczek <tpeczek@gmail.com>
2019-09-05 23:52:29 +0200

    Lib.AspNetCore.Mvc.Ndjson.NewtonsoftJson

---
+++ Lib.AspNetCore.Mvc.Ndjson.NewtonsoftJson/DependencyInjection/NdjsonStreamMvcBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Lib.AspNetCore.Mvc.Ndjson;
+using Lib.AspNetCore.Mvc.Ndjson.NewtonsoftJson;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions methods for configuring MVC via an <see cref="IMvcBuilder"/>.
+    /// </summary>
+    public static class NdjsonStreamMvcBuilderExtensions
+    {
+        /// <summary>
+        /// Configures NDJSON specific action result.
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcBuilder"/>.</param>
+        /// <returns>The <see cref="IMvcBuilder"/>.</returns>
+        public static IMvcBuilder AddNdjsonStreamResult(this IMvcBuilder builder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.TryAddSingleton<INdjsonWriterFactory, NewtonsoftNdjsonWriterFactory>();
+
+            return builder;
+        }
+    }
+}
---
+++ Lib.AspNetCore.Mvc.Ndjson.NewtonsoftJson/DependencyInjection/NdjsonStreamMvcCoreBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Lib.AspNetCore.Mvc.Ndjson;
+using Lib.AspNetCore.Mvc.Ndjson.NewtonsoftJson;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for adding Newtonsoft.Json to <see cref="IMvcCoreBuilder"/>.
+    /// </summary>
+    public static class NdjsonStreamMvcCoreBuilderExtensions
+    {
+        /// <summary>
+        /// Configures NDJSON specific action result.
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcBuilder"/>.</param>
+        /// <returns>The <see cref="IMvcBuilder"/>.</returns>
+        public static IMvcCoreBuilder AddNdjsonStreamResult(this IMvcCoreBuilder builder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.TryAddSingleton<INdjsonWriterFactory, NewtonsoftNdjsonWriterFactory>();
+
+            return builder;
+        }
+    }
+}
